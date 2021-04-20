cc4ce2b2f2334931cd5e0062bb0227b472dea764
55019 <55019@etu.he2b.be>
2021-04-21 00:19:42 +0200

    étape 1.6 : layer

---
+++ model/layer.js
@@ -0,0 +1,37 @@
+class Layer{
+    constructor(nbRows, nbColumns){
+        this.layer = Array(nbRows).fill().map(() => Array(nbColumns));
+    }
+
+    contains(pos){
+        let row = this.layer.length - 1;
+        let column = this.layer[0].length - 1;
+
+        return pos.getRow() >= 0 && pos.getRow() <= row && pos.getColumn() >= 0 && pos.getColumn() <= column;
+    }
+
+    setTile(position, tile){
+        if(this.contains(position) == false){
+            throw 'error position';
+        }
+        this.layer[position.getRow()][position.getColumn()] = tile;
+    }
+
+    getTile(position){
+        if(this.contains(position) == false){
+            throw 'error position';
+        }
+        return this.layer[position.getRow()][position.getColumn()];
+    }
+
+    hasTile(position){
+        if(this.contains(position) == false){
+            throw 'error position';
+        }
+        if(this.getTile(position) == undefined){
+            return false;
+        }
+        return true;
+    }
+    
+}
