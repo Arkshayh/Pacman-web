e09ac4d594e455a8877c3cee97d737d271c54edf
55019 <55019@etu.he2b.be>
2021-04-22 19:09:04 +0200

    étape 2.3 : refactoring Position

--- model/direction.js
+++ model/direction.js
@@ -16,7 +16,7 @@ class Direction{
     }
 }
 
-Direction.NORTH = new Direction(0,-1);
-Direction.SOUTH = new Direction(0,1);
-Direction.WEST = new Direction(-1,0);
-Direction.EAST = new Direction(1,0);
+Direction.NORTH = new Direction(-1,0);
+Direction.SOUTH = new Direction(1,0);
+Direction.WEST = new Direction(0,-1);
+Direction.EAST = new Direction(0,1);
--- model/position.js
+++ model/position.js
@@ -11,4 +11,14 @@ class Position {
     getColumn(){
         return this.column;
     }
+
+    /**
+     * returns to new position according to the given direction.
+     * @param {*Direction} direction a direction (NORTH/SOUTH/EAST/WEST)
+     * @returns {Position} pos
+     */
+    nextPosition(direction){
+        let pos = new Position(this.row + direction.getDeltarow(), this.column + direction.getDeltaColumn());
+        return pos;
+    }
 }
