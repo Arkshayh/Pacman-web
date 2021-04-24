73f86935ccedc6ef577fbcac4470fc01e16e2782
55019 <55019@etu.he2b.be>
2021-04-25 00:08:20 +0200

    étape 2.6 : Pacman

--- view/pacmanView.js
+++ view/pacmanView.js
@@ -1,7 +1,33 @@
 class PacmanView{
     constructor(_pacmanCtrl){
         this.pacmanCtrl = _pacmanCtrl;
+    }   
+
+    getPacCtrl(){
+        return this.pacmanCtrl;
     }
-    
 }
 
+
+    
+let pac;
+
+document.addEventListener("keydown", function (event) {
+    let touche = event.key;
+    if(PacmanView && pac instanceof PacmanView){
+        switch(touche){
+            case 'ArrowLeft':
+                pac.pacmanCtrl.askToChangeDirection(Direction.WEST);
+                break;
+            case 'ArrowRight':
+                pac.pacmanCtrl.askToChangeDirection(Direction.EAST);
+                break;
+            case 'ArrowUp':
+                pac.pacmanCtrl.askToChangeDirection(Direction.NORTH);
+                break;
+            case 'ArrowDown':
+                pac.pacmanCtrl.askToChangeDirection(Direction.SOUTH);
+                break;
+        }
+    }
+})
