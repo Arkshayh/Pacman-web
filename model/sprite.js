17bd0457b8eb9395015c1e7dedd612353fb403b3
55019 <55019@etu.he2b.be>
2021-04-24 14:27:03 +0200

    étape 2.4 : sprites

--- model/sprite.js
+++ model/sprite.js
@@ -15,11 +15,27 @@ class Sprite extends Component{
         return this.direction;
     }
 
+    getAskedDirection(){
+        return this.askedDirection;
+    }
+
     hasAskedToChangeDirection(){
         return this.askedToChangeDirection;
     }
 
-    getAskedDirection(){
-        return this.askedDirection;
+    move(){
+        let dir = this.getDirection();
+        let pos = this.getPosition();
+        this.position  = pos.nextPosition(dir);
+    }
+
+    askToChangeDirection(direction){
+        this.askedToChangeDirection = true;
+        this.askedDirection = direction;
+    }
+
+    changeDirection(){
+        this.direction = this.getAskedDirection();
+        this.askedToChangeDirection = false;
     }
 }
